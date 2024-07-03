5e5d67def0aa3bc60b6b8f83fb50122b04f42dd2
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-07-03 19:02:51 +0300

    ADD

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    await fs.appendFile(PATH_DB, data, 'utf8');
+    console.log('Дані успішно додані до файлу.');
+  } catch (err) {
+    console.error('Помилка додавання даних до файлу:', err);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,27 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let contacts = [];
+  //   async read data
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    contacts = JSON.parse(data);
+    console.log('Content of the file:', data);
+  } catch (error) {
+    console.error('Error reading the file:', error);
+  }
+  //   add contacts
+  for (let i = 0; i < number; i++) {
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+  }
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log('Add contact');
+  } catch (error) {
+    console.error('Mistake:', error);
+  }
+};
 
 generateContacts(5);
